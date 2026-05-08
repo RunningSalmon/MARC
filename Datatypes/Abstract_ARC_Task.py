fb3d644a1b52d034daab29bacdfb580910918691
RunningSalmon <lhgl@web.de>
2026-05-08 15:40:22 +0200

    implemented an abstract representation for ARC-Tasks using abstract objects

---
+++ Datatypes/Abstract_ARC_Task.py
@@ -0,0 +1,32 @@
+from Abstract_Object import *
+
+class AbstractObjectMatrix:
+    height: int
+    width: int
+    abstract_objects: list[AbstractObject]
+    def __init__(self, height: int, width: int, abstract_objects: list[AbstractObject]):
+        self.height = height
+        self.width = width
+        self.abstract_objects = abstract_objects
+
+    def __str__(self):
+        return f"An Abstract Matrix of Height: {self.height}, Width: {self.width}, containing the abstract objects {self.abstract_objects}"
+
+class AbstractObjectMatrixPair:
+    input = AbstractObjectMatrix
+    output = AbstractObjectMatrix
+
+    def __str__(self):
+        return (f"AbstractObjectMatrixPair with Input:\n{self.input},\n "
+                f"and Output:\n {self.output}\n")
+
+class AbstractARCTask:
+    train: list[AbstractObjectMatrixPair]
+    test: list[AbstractObjectMatrixPair]
+
+    def __repr__(self):
+        return (f"AbstractObjectARCTask(\n"
+                f"train:\n"
+                f"{self.train},\n"
+                f" test:\n"
+                f"{self.test}\n)")
